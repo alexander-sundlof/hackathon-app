e2f66170e8c4a3152a98da5c03e9727d4620dc11
Samuel Lindberg <samuel.lindberg@omegapoint.se>
2026-02-09 10:00:53 +0100

    Dog page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import DogImage from './DogImage'
+
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  
   return (
     <>
       <div>
@@ -16,7 +22,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <div>
+        <DogImage />
+      </div>
+
+      
+      <h1>FBI MOST WANTED </h1>
+      <h1> Guess the wanted sum. </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ my-react-app/src/DogImage.jsx
@@ -0,0 +1,5 @@
+export default function DogImage() { 
+    return ( <img src="https://place.dog/300/200"
+                  alt="Random dog"
+                  style={{ borderRadius: "8px" }} 
+            /> ) }
